e8f54ca4c5136d50d785806e24c15516d7bd5463
Brandon Hill <bhill@brandons-air.attlocal.net>
2017-04-02 10:41:21 -0700

    Render styling of album detail component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <View></View>
+    <View style={styles.containerStyle}>
+      { props.children }
+    </View>
   );
 };
 
 const styles = {
   containerStyle: {
-    
+    borderWidth: 1,
+    borderRadius: 2, // rounded corners
+    borderColor: '#ddd', // gray color
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2}, // no shadow on left or right side, but some on border
+    shadowOpacity: 0.1, // makes a component or item see-through. this will lighten the shadow
+    shadowRadius: 2, // just like border radius. will round out the shadow corners
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
   }
 };
 
